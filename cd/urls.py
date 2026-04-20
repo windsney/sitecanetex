6de83a3d484e0b2e9732ded97d04f9cff4542256
windsney <windsney@hotmail.com>
2026-04-20 12:11:16 -0400

    criando urls do cd

--- cd/urls.py
+++ cd/urls.py
@@ -10,8 +10,11 @@ app_name='cd'
 urlpatterns = [
 
     
-    
+    #path('criar-sindicancia/', inicio.views.criar_sindicancia, name='criar_sindicancia'),
     path('cd_cadastradas/', Home.as_view(),name='cd_cadastradas'),
+    #path('sindicancia/editar_sindicancia/<int:id>', inicio.views.editar_sindicancia, name='editar_sindicancia'),
+    #path('sindicancia/excluir_sindicancia/<int:id>', inicio.views.excluir_sindicancia, name='excluir_sindicancia'),
+   
     
 
 ]
