6c1ad08945402012373487d48fae465cc603f50f
windsney <windsney@hotmail.com>
2026-06-24 09:50:45 -0300

    mudou o debug

--- nome/settings.py
+++ nome/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1pi2z^&y%qo-q9&z$znjxk98)5z2rvb)&z*jawt#21yv9q9&-1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['padraopm.com.br', 'www.padraopm.com.br', '177.153.69.150','*']
+ALLOWED_HOSTS = ['padraopm.com.br', 'www.padraopm.com.br', '177.153.69.150']
 
 # ADICIONE ESTA LINHA EXATAMENTE ASSIM:
 CSRF_TRUSTED_ORIGINS = ['https://padraopm.com.br', 'https://www.padraopm.com.br']
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'nome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -109,7 +109,7 @@ DATABASES = {
     }
 }
 
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
